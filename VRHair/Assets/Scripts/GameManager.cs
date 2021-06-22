2dc6f501634a8acf8b24039878350f74789136ce
iamsherry000 <iamsherry000@gmail.com>
2021-06-22 17:31:57 +0800

    Sherry0622_1731

---
+++ VRHair/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+       // GameObject.Find("RightHand").GetComponent<drawer3>().enabled = false;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ VRHair/Assets/Scripts/ShowPanel/PanelMain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelMain : MonoBehaviour
+{
+    public void PaintClick() {
+        GameObject.Find("RightHand").GetComponent<drawer3>().enabled = true;
+        UIManager.Instance.ShowPanel("RPanel_Paint");
+    }
+}
